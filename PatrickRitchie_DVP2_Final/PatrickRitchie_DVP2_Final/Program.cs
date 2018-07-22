0a0a07e29a797ca9393b881ec2609552598d3b79
Patrick M Ritchie <pmritchie@student.fullsail.edu>
2018-07-22 16:30:10 -0400

    Create PlayGame and MainMenu function

--- PatrickRitchie_DVP2_Final/PatrickRitchie_DVP2_Final/Program.cs
+++ PatrickRitchie_DVP2_Final/PatrickRitchie_DVP2_Final/Program.cs
@@ -13,49 +13,9 @@ namespace PatrickRitchie_DVP2_Final
         {
             bool programIsRunning = true;
             Player currentPlayer = new Player();
-            //loop through main menu
-            while (programIsRunning)
-            {
-                Console.WriteLine("Let's play some Card Roulette with HAL from 2001: A Space Odyssey!\n" +
-                    "1: New User\n" +
-                    "2: Existing User\n" +
-                    "3: Exit\n");
-                //user selection
-                int userChoice = Validation.GetInt(1, 3, "Select from the options above: ");
-                switch (userChoice)
-                {
-                    case 1:
-                        {
-                            //creates player and saves player data in JSON
-                            string newPlayer = Validation.WhiteNull("Enter Player Name: ");
-                            currentPlayer.Name = newPlayer;
-
-                            int credits = 500;
-                            currentPlayer.Credits = credits;
-
-                            Console.WriteLine($"Welcome, {currentPlayer.Name}! You are starting with {currentPlayer.Credits} credits!\n" +
-                                "Let's Play!");
-                            SavePlayerJSON(currentPlayer);
-                        }
-                        break;
-                    case 2:
-                        {
-                            //load player data and choose player
-
-                            LoadJsonPlayers();
-
-
-                        }
-                        break;
-                    case 3:
-                        {
-                            programIsRunning = false;
-                            Utility.PauseBeforeContinuing();
-                        }
-                        break;
-
-                }
-            }
+            MainMenu();
+           
+           
 
 
 
@@ -140,11 +100,14 @@ namespace PatrickRitchie_DVP2_Final
 
             void PlayGame()
             {
-                int moves = 0;
-                while (true)
+                bool runGame = true;
+                while (runGame)
                 {
+                    // make deck of cards, suffle and deal cards
+                    int moves = 0;
                     PlayingDeck mainDeck = new PlayingDeck();
                     mainDeck.newDeck();
+                    mainDeck.shuffle();
 
                     PlayingDeck player = new PlayingDeck();
                     PlayingDeck computer = new PlayingDeck();
@@ -162,10 +125,10 @@ namespace PatrickRitchie_DVP2_Final
                         }
                         boo = !boo;
                     }
-
-                    while(!player.isEmpty() && computer.isEmpty())
+                    // loop to keep playing game
+                    while(!player.isEmpty() && !computer.isEmpty())
                     {
-
+                        
                         PlayingCard playerPick = (PlayingCard)player.draw();
                         PlayingCard computerPick = (PlayingCard)computer.draw();
                         moves++;
@@ -174,48 +137,142 @@ namespace PatrickRitchie_DVP2_Final
                             "How much would you like to bet?");
                         int playerBet = Validation.GetInt(0, currentPlayer.Credits,$"Place a bet between $0 and ${currentPlayer.Credits}");
                         int computerMatch = playerBet;
+                        int thePot = playerBet + computerMatch;
                         Console.WriteLine($"HAL has drawn {computerPick.face} of { computerPick.suit}!");
                         
 
                         if((int)playerPick.face >(int)computerPick.face)
                         {
-                            Console.WriteLine(" You won! You beat HAL this round!");
-                            playerBet += computerMatch;
-                            currentPlayer.Credits += playerBet;
-
+                            currentPlayer.Credits += thePot;
+                            Console.WriteLine(" You won! You beat HAL this round!\n"+
+                                $"You have won {thePot} credits this round\n\n"+
+                                $"Total Credits: {currentPlayer.Credits}");
                         }
                         else if((int) playerPick.face < (int)computerPick.face)
                         {
-                            Console.WriteLine("HAL won! Don't let HAL win anymore!");
-                            computerMatch += playerBet;
-                            currentPlayer.Credits -= computerMatch;
+                            
+                            currentPlayer.Credits -= thePot;
+                            Console.WriteLine("HAL won! Don't let HAL win anymore!" +
+                                $"You lost {thePot} credits this round.\n\n" +
+                                $"Total Credits: {currentPlayer.Credits}");
                         }
-                        else
+                        else// incase of a draw, double or nothing
                         {
                             Console.WriteLine("Oh look a draw!! Double or nothing now!");
                             while((int) playerPick.face == (int)computerPick.face)
                             {
                                 playerPick = (PlayingCard)player.draw();
                                 computerPick = (PlayingCard)computer.draw();
+                                moves++;
+                                thePot = thePot * 2;
+                                Console.WriteLine($"You have drawn {playerPick.face} of {playerPick.suit}!\n"+
+                                    $"Hal has drawn a {computerPick.face} of {computerPick.suit}\n\n");
 
                                 if ((int)playerPick.face > (int)computerPick.face)
                                 {
-                                    Console.WriteLine(" You won! You beat HAL this round!");
-                                    
+                                    currentPlayer.Credits += thePot;
+                                    Console.WriteLine(" You won! You beat HAL this round!\n" +
+                                        $"You have won {thePot} credits this round\n\n" +
+                                        $"Total Credits: {currentPlayer.Credits}");
+
                                 }
                                 else if ((int)playerPick.face < (int)computerPick.face)
                                 {
-                                    Console.WriteLine("HAL won! Don't let HAL win anymore!");
+                                    currentPlayer.Credits -= thePot;
+                                    Console.WriteLine("HAL won! Don't let HAL win anymore!" +
+                                         $"You lost {playerBet *2} credits this round.\n\n" +
+                                         $"Total Credits: {currentPlayer.Credits}");
                                 }
                             }
                             
 
+                        }
+                        //to continue, main menu or quit
+                        Console.WriteLine("Continue?\n\n"+
+                            "1: Yes\n"+
+                            "2: Main Menu\n"+
+                            "3: Save and Quit\n");
+
+                        int choice = Validation.GetInt(1, 3, "Choose an option above\n");
+                        switch (choice)
+                        {
+                            case 1:
+                                {
+                                    Console.WriteLine("Alright, let's keep going!");
+                                }break;
+                            case 2:
+                                {
+                                    SavePlayerJSON(currentPlayer);
+                                    runGame = false;
+                                    MainMenu();
+                                }break;
+                            case 3:
+                                {
+                                    SavePlayerJSON(currentPlayer);
+                                    runGame = false;
+                                    //add quit app
+                                }break;
                         }
 
                     }
                 }
 
                 
+            }
+
+            void MainMenu()
+            {
+          
+                //loop through main menu
+                while (programIsRunning)
+                {
+                    Console.WriteLine("Let's play some Card Roulette with HAL from 2001: A Space Odyssey!\n" +
+                        "1: New User\n" +
+                        "2: Existing User\n" +
+                        "3: Exit\n");
+                    //user selection
+                    int userChoice = Validation.GetInt(1, 3, "Select from the options above: ");
+                    Console.Clear();
+                    switch (userChoice)
+                    {
+                        case 1:
+                            {
+                                //creates player and saves player data in JSON
+                                string newPlayer = Validation.WhiteNull("Enter Player Name: ");
+                                currentPlayer.Name = newPlayer;
+
+                                int credits = 500;
+                                currentPlayer.Credits = credits;
+
+                                Console.WriteLine($"Welcome, {currentPlayer.Name}! You are starting with {currentPlayer.Credits} credits!\n" +
+                                    "Let's Play!");
+
+
+                                PlayGame();
+                                SavePlayerJSON(currentPlayer);
+                                Console.Clear();
+                            }
+                            break;
+                        case 2:
+                            {
+                                //load player data and choose player
+
+                                LoadJsonPlayers();
+                                PlayGame();
+
+                                Console.Clear();
+
+                            }
+                            break;
+                        case 3:
+                            {
+                                programIsRunning = false;
+                                Utility.PauseBeforeContinuing();
+                            }
+                            break;
+
+                    }
+                }
             }
             
         }
